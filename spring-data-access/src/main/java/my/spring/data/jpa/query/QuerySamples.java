692400cc8d46571ac30473fd6ea8db2361e2ed2c
PhilipBukin <philip.bukins@gmail.com>
2015-07-02 09:02:01 +0300

    Some adds in query samples of Spring Data

--- spring-data-access/src/main/java/my/spring/data/jpa/query/QuerySamples.java
+++ spring-data-access/src/main/java/my/spring/data/jpa/query/QuerySamples.java
@@ -1,11 +1,15 @@
 package my.spring.data.jpa.query;
 
 import my.spring.data.jpa.Person;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 
+import javax.persistence.QueryHint;
 import java.util.List;
 
 /**
@@ -34,13 +38,24 @@ interface PersonRepo extends JpaRepository<Person, Long> {
      * Если имя не обьявленно в Entity будет использованно нозвание класса.
      * Такой подход упращает работу с параметризированными репозиториями
      */
-    @Query("SELECT p FROM #{entityName} p WHERE p.name = :name")
+    @Query("SELECT p FROM #{#entityName} p WHERE p.name = :name")
     public List<Person> findByNameSpringEL(@Param( "name" ) String name);
 
 
+    /**
+     * Обновление
+     */
     @Modifying
     @Query("update Person u set u.age = ?1 where u.name = ?2")
     int setFixedAgeOnName(int age, String name);
+
+
+    /**
+     *Указать доп. информации при выполнении запроса + флаг что указывает не выполняться при запросе на количество
+     * сущностей для определение страниц
+     */
+    @QueryHints(value = { @QueryHint( name = "name", value = "value")}, forCounting = false)
+    Page<Person> findByName3(String name, Pageable pageable);
 }
 
 
